9fe03ded40c27b9f5fabf5ea4e88e86140ffa62c
Paúl <paulsolanocr@gmail.com>
2026-08-04 07:26:42 -0600

    Remove underscore prefix from constructor parameters

--- Python Intermedio/Extra de Los 4 Pilares de OOP Python Intermedio/Ejercicio 3.py
+++ Python Intermedio/Extra de Los 4 Pilares de OOP Python Intermedio/Ejercicio 3.py
@@ -1,26 +1,26 @@
 class Vehicle:
-    def __init__(self, _brand, _year):
-        self._brand = _brand
-        self._year = _year
+    def __init__(self, brand, year):
+        self._brand = brand
+        self._year = year
 
     def get_info(self):
         return f"Brand: {self._brand}, Year: {self._year}"
 
 class Car(Vehicle):
-    def __init__(self, _brand, _year, _doors, _engine):
-        super().__init__(_brand, _year)
-        self.doors = _doors
-        self.engine = _engine
+    def __init__(self, brand, year, doors, engine):
+        super().__init__(brand, year)
+        self.doors = doors
+        self.engine = engine
 
     def get_info(self):
         base_info = super().get_info()
         return f"{base_info}, Doors: {self.doors}, Engine: {self.engine}"
 
 class Motorcycle(Vehicle):
-    def __init__(self, _brand, _year, _type, _wheels):
-        super().__init__(_brand, _year)
-        self.type = _type
-        self.wheels = _wheels
+    def __init__(self, brand, year, type, wheels):
+        super().__init__(brand, year)
+        self.type = type
+        self.wheels = wheels
 
     def get_info(self):
         base_info = super().get_info()
